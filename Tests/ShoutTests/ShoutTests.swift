2e17e733f077a4bac8545547575955a7d67a895e
Tomas Green <tomas.green@gmail.com>
2021-12-20 10:58:54 +0100

    Update ShoutTests.swift

--- Tests/ShoutTests/ShoutTests.swift
+++ Tests/ShoutTests/ShoutTests.swift
@@ -9,6 +9,10 @@ final class ShoutTests: XCTestCase {
         let expectation = XCTestExpectation(description: "testDatabase")
         let s = Shout("Testing")
         s.publisher.sink { event in
+            XCTAssert(event.message == "[\"test\"]")
+            XCTAssert(event.level == .info)
+            XCTAssert(event.filename.hasSuffix("ShoutTests.swift"))
+            XCTAssert(event.function == "testExample()")
             expectation.fulfill()
         }.store(in: &cancellables)
         s.info("test")
